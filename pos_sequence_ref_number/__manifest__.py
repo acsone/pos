8bf2a8f0d0e18adda278a76012d99a592846945f
gilles <meyomesse.gilles@gmail.com>
2017-05-24 19:11:23 +0200

    [MIG] pos_sequence_ref_number

--- pos_sequence_ref_number/__manifest__.py
+++ pos_sequence_ref_number/__manifest__.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-# © 2016 Acsone SA/NV (http://www.acsone.eu)
-# © 2016 Eficent Business and IT Consulting Services S.L.
-# (http://www.eficent.com)
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+# Copyright 2016-2017 Acsone SA/NV (http://www.acsone.eu) and
+# Eficent Business and IT Consulting Services S.L (http://www.eficent.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'POS Sequence Ref Number',
-    'version': '8.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': 'Point Of Sale',
     'sequence': 1,
     'author': "Eficent Business and IT Consulting Services,"
@@ -22,6 +21,4 @@
         'static/src/xml/pos.xml'
     ],
     'installable': True,
-    'application': False,
-    'auto_install': False,
 }

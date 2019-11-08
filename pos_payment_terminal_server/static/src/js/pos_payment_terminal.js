03376d62b394bd0833b0365b2d3101b277af00a0
Denis Roussel <denis.roussel@acsone.eu>
2019-11-08 14:51:23 +0100

    !fixup

--- pos_payment_terminal_server/static/src/js/pos_payment_terminal.js
+++ pos_payment_terminal_server/static/src/js/pos_payment_terminal.js
@@ -18,12 +18,8 @@ odoo.define('pos_payment_terminal_server.pos_payment_terminal', function (requir
                 var self = this;
             }
         },
-        on_click_transaction_start: function(event){
-            // TODO: Remove this and make mechanism in pos_payment_terminal
+        start_transaction: function(line_cid){
             var self = this;
-            var line_cid = $(event.currentTarget).data('cid');
-            self.hide_transaction_started(line_cid);
-            self.order_changes();
             self.pos.terminal_server.payment_terminal_transaction_start(line_cid, self.pos.currency.name, self.pos.currency.decimals);
         }
     });

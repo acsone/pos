ced077d100f10675bc46b7f8f6d4ec7bd7121413
Quentin Groulard <quentin.groulard@acsone.eu>
2021-04-07 11:43:45 +0200

    [FIX] multi_price override pricelist items priority in pos

--- pos_product_multi_price/static/src/js/models.js
+++ pos_product_multi_price/static/src/js/models.js
@@ -2,22 +2,32 @@ odoo.define("pos_product_multi_price.models", function(require) {
     "use strict";
 
     var models = require("point_of_sale.models");
+    var core = require("web.core");
     var utils = require("web.utils");
 
     var round_pr = utils.round_precision;
+    var _t = core._t;
 
     models.load_fields("product.product", ["price_ids_json"]);
 
-    var _product_super = models.Product.prototype;
     models.Product = models.Product.extend({
         get_price: function(pricelist, quantity) {
             // Partially copied from "point_of_sale.models"
 
             var self = this;
             var price_ids_json = JSON.parse(this.price_ids_json);
-            var price = _product_super.get_price.apply(this, arguments);
+            var price = self.lst_price;
 
             var date = moment().startOf("day");
+
+            if (pricelist === undefined) {
+                alert(
+                    _t(
+                        "An error occurred when loading product prices. " +
+                            "Make sure all pricelists are available in the POS."
+                    )
+                );
+            }
 
             var category_ids = [];
             var category = this.categ;
@@ -40,7 +50,23 @@ odoo.define("pos_product_multi_price.models", function(require) {
                 if (rule.min_quantity && quantity < rule.min_quantity) {
                     return false;
                 }
-                if (rule.base === "multi_price" && rule.compute_price === "formula") {
+
+                if (rule.base === "pricelist") {
+                    price = self.get_price(rule.base_pricelist, quantity);
+                } else if (rule.base === "standard_price") {
+                    price = self.standard_price;
+                }
+
+                if (rule.compute_price === "fixed") {
+                    price = rule.fixed_price;
+                    return true;
+                } else if (rule.compute_price === "percentage") {
+                    price -= price * (rule.percent_price / 100);
+                    return true;
+                } else if (
+                    rule.base === "multi_price" &&
+                    rule.compute_price === "formula"
+                ) {
                     _.forEach(price_ids_json, function(multi_price) {
                         if (multi_price.price_id === rule.multi_price_name[0]) {
                             price = multi_price.price;
@@ -67,7 +93,24 @@ odoo.define("pos_product_multi_price.models", function(require) {
                             return true;
                         }
                     });
+                } else {
+                    var price_limit = price;
+                    price -= price * (rule.price_discount / 100);
+                    if (rule.price_round) {
+                        price = round_pr(price, rule.price_round);
+                    }
+                    if (rule.price_surcharge) {
+                        price += rule.price_surcharge;
+                    }
+                    if (rule.price_min_margin) {
+                        price = Math.max(price, price_limit + rule.price_min_margin);
+                    }
+                    if (rule.price_max_margin) {
+                        price = Math.min(price, price_limit + rule.price_max_margin);
+                    }
+                    return true;
                 }
+
                 return false;
             });
             return price;

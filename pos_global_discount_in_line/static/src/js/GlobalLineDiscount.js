4bd9a2dc6c6578a155321816e1f38218971de428
Maxime Franco <maxime.franco@acsone.eu>
2023-02-22 14:51:54 +0100

    [MIG] pos_global_discount_in_line: Migration to 16.0

--- pos_global_discount_in_line/static/src/js/GlobalLineDiscount.js
+++ pos_global_discount_in_line/static/src/js/GlobalLineDiscount.js
@@ -2,37 +2,37 @@ odoo.define("pos_global_discount_in_line.GlobalLineDiscount", function (require)
 
 
     const DiscountButton = require("pos_discount.DiscountButton");
+    const ProductScreen = require('point_of_sale.ProductScreen');
     const Registries = require("point_of_sale.Registries");
 
-    const GlobalLineDiscount = (DiscountButton) =>
-        class extends DiscountButton {
-            async onClick() {
-                // Replaced to remove rounding
-                var self = this;
-                const {confirmed, payload} = await this.showPopup("NumberPopup", {
-                    title: this.env._t("Discount Percentage"),
-                    startingValue: this.env.pos.config.discount_pc,
-                });
-                if (confirmed) {
-                    await self.apply_discount(payload.replace(",", "."));
-                }
+    class GlobalLineDiscount extends DiscountButton {
+        async onClick() {
+            // Replaced to remove rounding
+            var self = this;
+            const {confirmed, payload} = await this.showPopup("NumberPopup", {
+                title: this.env._t("Discount Percentage"),
+                startingValue: this.env.pos.config.discount_pc,
+            });
+            if (confirmed) {
+                await self.apply_discount(payload.replace(",", "."));
             }
-            async apply_discount(pc) {
-                if (this.env.pos.config.global_discount_in_line) {
-                    var order = this.env.pos.get_order();
-                    var lines = order.get_orderlines();
-                    for (const ind in lines) {
-                        lines[ind].discount = pc;
-                        lines[ind].discountStr = String(pc);
-                    }
-                    order.deselect_orderline();
-                } else {
-                    super.apply_discount(pc);
+        }
+        async apply_discount(pc) {
+            if (this.env.pos.config.global_discount_in_line) {
+                var order = this.env.pos.get_order();
+                var lines = order.get_orderlines();
+                for (const ind in lines) {
+                    lines[ind].discount = pc;
+                    lines[ind].discountStr = String(pc);
                 }
+                order.deselect_orderline();
+            } else {
+                super.apply_discount(pc);
             }
-        };
+        }
+    };
 
-    Registries.Component.extend(DiscountButton, GlobalLineDiscount);
+    Registries.Component.add(GlobalLineDiscount);
 
-    return DiscountButton;
+    return GlobalLineDiscount;
 });
